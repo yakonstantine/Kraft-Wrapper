380bc485a34c7ee2718ea4df7cebed0e6e61947d
Konstantin Yakovlev <yakonstantine@gmail.com>
2017-12-28 18:46:53 +0300

    Added convertion to Sitecore ID string

---
+++ KraftWrapper/KraftWrapper.Extensions.Tests/SitecoreIDExpensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KraftWrapper.Extensions.Tests
+{
+    [TestClass]
+    public class SitecoreIDExpensionsTests
+    {
+        [TestMethod]
+        public void CheckConvertionToSitecoreIDString()
+        {
+            var giud = new Guid("00000000-aaaa-BBBB-cccc-000000001234");
+
+            var result = giud.ToIDString();
+
+            Assert.AreEqual("{00000000-AAAA-BBBB-CCCC-000000001234}", result);
+        }
+    }
+}
---
+++ KraftWrapper/KraftWrapper/Extensions/SitecoreIDExpensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KraftWrapper.Extensions
+{
+    public static class SitecoreIDExpensions
+    {
+        public static string ToIDString(this Guid id)
+        {
+            return $"{{{id.ToString().ToUpper()}}}";
+        }
+    }
+}
