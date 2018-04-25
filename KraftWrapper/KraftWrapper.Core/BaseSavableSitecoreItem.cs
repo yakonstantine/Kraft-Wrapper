6d0bd08d1a44242214a5925fe76c4038c55717c3
Konstantin Yakovlev <yakonstantine@gmail.com>
2018-04-25 11:11:03 +0300

    Implemented base class for SavableSitecoreItem

---
+++ KraftWrapper/KraftWrapper.Core/BaseSavableSitecoreItem.cs
@@ -0,0 +1,114 @@
+﻿using KraftWrapper.Interfaces;
+using KraftWrapper.Options;
+using System;
+
+namespace KraftWrapper.Core
+{
+    public abstract class BaseSavableSitecoreItem : ISavableSitecoreItem
+    {
+        protected readonly ISitecoreConfiguration _sitecoreConfiguration;
+        protected readonly ISitecoreFactory _sitecoreFactory;
+        protected readonly ISitecorePublisher _sitecorePublisher;
+
+        private ISitecoreDatabase _masterDb;
+
+        protected BaseSavableSitecoreItem(
+            ISitecoreFactory sitecoreFactory,
+            ISitecoreConfiguration sitecoreConfiguration,
+            ISitecorePublisher sitecorePublisher)
+        {
+            _sitecoreFactory = sitecoreFactory;
+            _sitecoreConfiguration = sitecoreConfiguration;
+            _sitecorePublisher = sitecorePublisher;
+        }
+
+        public abstract ISitecoreItem SourceItem { get; protected set; }
+        public abstract ISitecoreLanguage Language { get; protected set; }
+        protected ISitecoreDatabase MasterDb
+        {
+            get
+            {
+                if (_masterDb == null)
+                {
+                    _masterDb = _sitecoreFactory.GetDatabase("master");
+                }
+
+                return _masterDb;
+            }
+        }
+
+        public void Save()
+        {
+            if (this.Language == null)
+            {
+                throw new NullReferenceException("The language is not initialized.");
+            }
+
+            _sitecoreConfiguration.DisableSecurity();
+            _sitecoreConfiguration.SwitchLanguage(this.Language);
+
+            if (this.SourceItem == null)
+            {
+                CreateNewSourceItem();
+            }
+
+            this.SourceItem.BeginEdit();
+
+            try
+            {
+                SetSourceItemFieldValues();
+
+                this.SourceItem.EndEdit();
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error($"Could not update item {this.SourceItem.FullPath}: {ex.Message}", this);
+
+                this.SourceItem.CancelEdit();
+
+                throw;
+            }
+            finally
+            {
+                _sitecoreConfiguration.EnableSecurity();
+                _sitecoreConfiguration.ResetLanguage();
+            }
+        }
+
+        public void Publish(PublishMode publishMode, bool deep, bool withRelatedItems)
+        {
+            if (this.SourceItem == null)
+            {
+                throw new NullReferenceException("The target sitecore item doesn't created or set");
+            }
+
+            if (this.Language == null)
+            {
+                throw new NullReferenceException("The language is not initialized.");
+            }
+
+            try
+            {
+                var webDb = _sitecoreFactory.GetDatabase("web");
+
+                var publishOptions = new PublishOptions(this.MasterDb, webDb, this.SourceItem, this.Language)
+                {
+                    Mode = publishMode,
+                    Deep = deep,
+                    PublishRelatedItems = withRelatedItems
+                };
+
+                _sitecorePublisher.Publish(publishOptions);
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error("Exception publishing items from custom pipeline! : " + ex, this);
+                throw;
+            }
+        }
+
+        protected abstract void CreateNewSourceItem();
+
+        protected abstract void SetSourceItemFieldValues();
+    }
+}
---
+++ KraftWrapper/KraftWrapper/Interfaces/ISavableSitecoreItem.cs
@@ -0,0 +1,12 @@
+﻿using KraftWrapper.Options;
+
+namespace KraftWrapper.Interfaces
+{
+    public interface ISavableSitecoreItem
+    {
+        ISitecoreItem SourceItem { get; }
+        ISitecoreLanguage Language { get; }
+        void Save();
+        void Publish(PublishMode publishMode, bool deep, bool withRelatedItems);
+    }
+}
